a8153bb220e569251447edcc9e70ad137cac85f8
Myy <myy@miouyouyou.fr>
2017-07-23 15:17:06 +0000

    Updated the reboot patch and suffixed a -T to Tinkering images
    In order to differentiate them in the logs.
    
    Signed-off-by: Myy <myy@miouyouyou.fr>

--- GetPatchAndCompileKernel.sh
+++ GetPatchAndCompileKernel.sh
@@ -3,7 +3,7 @@ export CROSS_COMPILE=armv7a-hardfloat-linux-gnueabi-
 
 export KERNEL_SERIES=v4.13
 export KERNEL_BRANCH=v4.13-rc1
-export LOCALVERSION=-RockMyy-XIII
+export LOCALVERSION=-RockMyy-XIII-T
 export MALI_VERSION=r19p0-01rel0
 export MALI_BASE_URL=https://developer.arm.com/-/media/Files/downloads/mali-drivers/kernel/mali-midgard-gpu
 
