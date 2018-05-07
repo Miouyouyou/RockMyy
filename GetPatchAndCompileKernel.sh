1208f954265a551a3d8a8b23e00cc0cc7928a43d
Myy Miouyouyou <myy@miouyouyou.fr>
2018-05-07 20:40:53 +0200

    The v4.17-rc4.
    Nothing to report for this one.
    
    Signed-off-by: Myy Miouyouyou <myy@miouyouyou.fr>

--- GetPatchAndCompileKernel.sh
+++ GetPatchAndCompileKernel.sh
@@ -4,7 +4,7 @@ export CROSS_COMPILE=arm-linux-gnueabihf-
 export KERNEL_GIT_URL='git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git'
 
 export KERNEL_SERIES=v4.17
-export KERNEL_BRANCH=v4.17-rc3
+export KERNEL_BRANCH=v4.17-rc4
 export LOCALVERSION=-RockMyy-Seventeen
 export MALI_VERSION=r19p0-01rel0
 export MALI_BASE_URL=https://developer.arm.com/-/media/Files/downloads/mali-drivers/kernel/mali-midgard-gpu
