cbb73c2e93d942af650544c43db5f70c86822a7f
Miouyouyou (Myy) <myy@miouyouyou.fr>
2019-03-10 19:56:27 +0100

    The 5.0 !
    WITH...OUT the VPU.
    Yes, 2 years after it's still a fucking mess to get anything done
    with the VPU, on those platforms.
    And I'm not going to comment how half of the patches now just
    completely ignore the RK3288 boards, focusing on the RK3399.
    
    I'm not giving up but I'll need to get back to my driver to
    perform additional tests, since testing most of the V4L2 Request
    patches is insanely difficult and poorly documented, ATM.
    
    Still, I added in patches/SeriesToTest the patches series I need
    to test. Those don't compile because they, of course, depend on
    other unknown patches provided who knows where, who knows why...
    
    I'm becoming very salty when it comes to the VPU, since
    every development I've seen so far do not have a really testing
    suite. It's generally "Just take some random patches from FFMPEG
    here, and some patches for MPV there, and additional kernel
    patches on another place, and combine them so that it *could*
    *maybe* works in a some conditions."
    What file format is clearly supported ? Can I take any video
    using this format and it will work ? Is there a quick way to
    check if the VPU driver is correctly installed ?
    ... How do you guys work and test this thing ?
    
    Signed-off-by: Miouyouyou (Myy) <myy@miouyouyou.fr>

--- GetPatchAndCompileKernel.sh
+++ GetPatchAndCompileKernel.sh
@@ -8,8 +8,8 @@ if [ -z ${MAKEOPTS+x} ]; then
 fi
 
 export KERNEL_SERIES=v5.0
-export KERNEL_BRANCH=v5.0-rc7
-export LOCALVERSION=-RockMyy-HighFive-VPU-Beta
+export KERNEL_BRANCH=v5.0
+export LOCALVERSION=-RockMyy-HighFive
 export MALI_VERSION=r19p0-01rel0
 export MALI_BASE_URL=https://developer.arm.com/-/media/Files/downloads/mali-drivers/kernel/mali-midgard-gpu
 
@@ -64,66 +64,6 @@ export KERNEL_PATCHES="
 0011-block-partitions-efi-Ignore-bizarre-Chromebook-GPT-p.patch
 0012-mmc-Added-a-flag-to-disable-cache-flush-during-reset.patch
 0013-spi-Added-support-for-Tinkerboard-s-SPI-interface.patch
-0100-ARM-dts-rockchip-move-rk3036-i2s-sound-dail-cells-in.patch
-0101-ARM-dts-rockchip-add-HCLK_HDMI-to-rk3066-vio-power-d.patch
-0102-ARM-dts-rockchip-fix-cif1_pdn-pin-on-rk3188-bqedison.patch
-0103-ARM-dts-rockchip-add-focaltech-touchscreen-to-rk3188.patch
-0104-arm64-dts-rockchip-add-rk3328-ACODEC-node.patch
-0105-arm64-dts-rockchip-move-rk3328-sound-dai-cells-to-th.patch
-0106-arm64-dts-rockchip-enable-analog-audio-node-for-rock.patch
-0107-clk-rockchip-fix-frac-settings-of-GPLL-clock-for-rk3.patch
-0108-clk-rockchip-add-CLK_SET_RATE_PARENT-for-rk3066-lcdc.patch
-0109-arm64-dts-rockchip-Add-devicetree-for-NanoPC-T4.patch
-0110-ARM-dts-rockchip-add-rk3066-vop-display-nodes.patch
-0111-ARM-dts-rockchip-Add-missing-dma-names-SPI-support-f.patch
-0112-ARM-dts-rockchip-rv1108-Add-spim0-and-spim1-pinctrl-.patch
-0113-dt-bindings-Add-vendor-prefix-for-elgin.patch
-0114-ARM-dts-rv1108-Add-support-for-rv1108-elgin-r1-board.patch
-0115-arm64-dts-rockchip-add-ROCK-Pi-4-DTS-support.patch
-0116-arm64-dts-rockchip-Add-DT-for-NanoPi-M4.patch
-0117-arm64-dts-rockchip-Refine-nanopi4-differences.patch
-0118-arm64-dts-rockchip-Add-NanoPC-T4-IR-receiver.patch
-0119-arm64-dts-rockchip-Fix-nanopi4-uSD-card-detect.patch
-0120-ARM-dts-rockchip-clean-up-the-abuse-of-disable-wp.patch
-0121-arm64-dts-rockchip-clean-up-the-abuse-of-disable-wp.patch
-0122-media-v4l2-mem2mem-add-v4l2_m2m_buf_copy_data-helper.patch
-0123-media-vim2m-use-v4l2_m2m_buf_copy_data.patch
-0124-media-vicodec-use-v4l2_m2m_buf_copy_data.patch
-0125-media-buffer.rst-clean-up-timecode-documentation.patch
-0126-media-videodev2.h-add-v4l2_timeval_to_ns-inline-func.patch
-0127-media-vb2-add-vb2_find_timestamp.patch
-0128-media-cedrus-identify-buffers-by-timestamp.patch
-0129-media-extended-controls.rst-update-the-mpeg2-compoun.patch
-0130-media-vim2m-the-v4l2_m2m_buf_copy_data-args-were-swa.patch
-0131-media-vb2-vb2_find_timestamp-drop-restriction-on-buf.patch
-0132-media-vb2-Keep-dma-buf-buffers-mapped-until-they-are.patch
-0133-media-cedrus-Remove-completed-item-from-TODO-list-dm.patch
-0134-media-vb2-add-buf_out_validate-callback.patch
-0135-media-vim2m-add-buf_out_validate-callback.patch
-0136-media-vivid-add-buf_out_validate-callback.patch
-0137-media-cedrus-add-buf_out_validate-callback.patch
-0138-media-vb2-check-that-buf_out_validate-is-present.patch
-0139-media-videobuf2-remove-unused-variable.patch
-0140-media-vb2-Fix-buf_out_validate-documentation.patch
-0141-media-Introduce-helpers-to-fill-pixel-format-structs.patch
-0142-rockchip-vpu-Use-pixel-format-helpers.patch
-0143-rockchip-vpu-Use-v4l2_m2m_buf_copy_data.patch
-0144-rockchip-vpu-Cleanup-macroblock-alignment.patch
-0145-rockchip-vpu-Cleanup-JPEG-bounce-buffer-management.patch
-0146-rockchip-vpu-Open-code-media-controller-register.patch
-0147-rockchip-vpu-Support-the-Request-API.patch
-0148-rockchip-vpu-Add-decoder-boilerplate.patch
-0149-rockchip-vpu-Add-support-for-non-standard-controls.patch
-0150-rockchip-vpu-Add-support-for-MPEG-2-decoding.patch
-0151-rockchip-vpu-Add-support-for-RK3328.patch
-0152-rockchip-vpu-Add-support-for-MPEG-2-decoding-on-RK32.patch
-0153-clk-rockchip-rk3288-fix-vcodec-parent.patch
-0154-HACK-rockchip-vpu-register-decoder-as-first-video-de.patch
-0155-Revert-media-mpeg2-ctrls.h-move-MPEG2-state-controls.patch
-0156-arm64-dts-rockchip-add-rk3328-VPU-nodes.patch
-0157-clk-rockchip-fix-bad-clocks-on-rk3328.patch
-0158-drm-rockchip-vop-reset-scale-mode-when-win-is-disabl.patch
-0159-rockchip-vpu-add-runtime-pm-callbacks.patch
 "
 
 export KERNEL_DTS_PATCHES="
@@ -148,8 +88,6 @@ export KERNEL_DTS_PATCHES="
 0022-dts-rk3288-veyron-chromebook-dedicate-npll-to-VOP0-H.patch
 0023-dts-rk3288-support-for-dedicating-npll-to-a-vop.patch
 0024-arm-dts-veyron-Added-a-flag-to-disable-cache-flush-d.patch
-0025-ARM-DTSI-Setup-the-VEPU-and-VDPU-nodes-for-the-MPP-S.patch
-0026-ARM-DTS-Enable-the-VEPU-VDPU-and-VPU-MMU-on-the-Tink.patch
 "
 
 export KERNEL_DOCUMENTATION_PATCHES="
